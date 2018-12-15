7acade714c812318316eda3201c38d5c056f0e0b
shannenbravo <shannenbravo@gmail.com>
2018-12-15 16:46:20 -0500

    first commit

---
+++ TinderOnCrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderOnCrack
+//
+//  Created by Shannen Bravo-Brown on 12/15/18.
+//  Copyright © 2018 Shannen Bravo-Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
