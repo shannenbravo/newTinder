8d322b3f6fce7d7a5052a90379abce255ba6c580
shannenbravo <shannenbravo@gmail.com>
2018-12-17 11:05:24 -0500

    setting up ui

--- TinderOnCrack/ViewController.swift
+++ TinderOnCrack/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
+        
+        //red stack view crap
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map {
+            (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        let blueView = UIView()
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        blueView.backgroundColor = .blue
+        let stackView = UIStackView(arrangedSubviews: [topStackView,blueView, yellowView])
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        stackView.axis = .vertical
+        stackView.fillSuperview()
     }
 
 
