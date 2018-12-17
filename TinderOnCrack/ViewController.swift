00742bd52b5e62d2956bffab9971880ca03989c9
shannenbravo <shannenbravo@gmail.com>
2018-12-17 14:51:36 -0500

    added animation to uiview

--- TinderOnCrack/ViewController.swift
+++ TinderOnCrack/ViewController.swift
@@ -39,6 +39,8 @@ class ViewController: UIViewController {
         overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor)
         overallStackView.isLayoutMarginsRelativeArrangement = true
         overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+        overallStackView.bringSubviewToFront(cardsDeckViews)
     }
 
 
--- TinderOnCrack/Views/CardView.swift
+++ TinderOnCrack/Views/CardView.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class CardView: UIView {
+    // set temp image on card view
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "pictwo"))
     
-    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "profilepic"))
-    
+    //have to be overrdien
     override init(frame: CGRect) {
         super.init(frame: frame)
+        //makes it curve
         layer.cornerRadius = 10
-        clipsToBounds = true 
+        //make the curve show
+        clipsToBounds = true
+        //add the image to the ui
         addSubview(imageView)
+        //the anchores
         imageView.fillSuperview()
         
         //adding gestures
@@ -24,16 +29,42 @@ class CardView: UIView {
         addGestureRecognizer(panGesture)
     }
     
+  
+    
+    
     @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer){
         
-        let translation = gesture.translation(in: nil)
-            self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    
+        switch gesture.state {
+        //if the state of the card view changed
+        case .changed:
+            handleChangedSate(gesture)
+        //after the click had finished
+        case .ended:
+            handleEnded()
+        default:
+            ()
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    fileprivate func handleEnded() {
+        //butit in function to animate the ui
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            //go back to where it was before
+            self.transform = .identity
+        }) { (_) in
+        }
+    }
+    
+    fileprivate func handleChangedSate(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        //tracks the pan 
+        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
 
 
 }
